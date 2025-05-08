b801d9c81408203e7ddf4ae52037399b2618332e
Abhijeet019 <abhijeetsharma019@gmail.com>
2025-05-08 21:50:01 +0530

    changes

--- src/test/java/com/example/demo_func/DemoFuncApplicationTests.java
+++ src/test/java/com/example/demo_func/DemoFuncApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.demo_func;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DemoFuncApplicationTests {
 
 	@Test
